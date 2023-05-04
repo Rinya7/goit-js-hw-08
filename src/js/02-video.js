57105d852b96d81dca6db381b05b219878ebfeba
Rinya7 <v.dmitriev0712@gmail.com>
2023-05-04 21:40:49 +0200

    lesson2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const myPlayer = new Player(iframe);
+
+//const onPlay = function (data) {
+//  localStorage.setItem('videoplayer-current-time', data.seconds);
+//};
+
+myPlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+myPlayer.on(
+  'timeupdate',
+  throttle(
+    data => localStorage.setItem('videoplayer-current-time', data.seconds),
+    1000
+  )
+);
